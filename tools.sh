ecf10bbba3e42827a9013f6da7905ac1005b2014
Jeroen Scheeres <jscheeres@gmail.com>
2022-10-16 10:22:54 +0200

    Add slack, chrome and vscode

--- tools.sh
+++ tools.sh
@@ -4,3 +4,19 @@ APPS="maim"
 
 sudo apt-get update
 sudo apt install -y $APPS
+
+cd ~/Downloads
+
+# Install Slack
+wget https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
+sudo dpkg -i slack-desktop-4.28.184-amd64.deb
+
+# Install VSCode
+wget https://az764295.vo.msecnd.net/stable/d045a5eda657f4d7b676dedbfa7aab8207f8a075/code_1.72.2-1665614327_amd64.deb
+sudo dpkg -i code_1.72.2-1665614327_amd64.deb
+
+# Install Chrome
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
